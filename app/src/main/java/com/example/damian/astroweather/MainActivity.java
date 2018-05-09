55f55960ecda0114946bf1b3d5ca9b2f90709554
Damian Kubiak <kubiak.damian96@gmail.com>
2018-05-09 19:49:10 +0200

    Added attr for small device

--- app/src/main/java/com/example/damian/astroweather/MainActivity.java
+++ app/src/main/java/com/example/damian/astroweather/MainActivity.java
@@ -17,7 +17,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         tabLayout = findViewById(R.id.tablelayoutid);
-
         if(!getResources().getBoolean(R.bool.isTablet)){
             setAdapter();
         }
